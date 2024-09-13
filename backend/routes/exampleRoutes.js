084c101c1c79774fca748ab493034fd08d8f8a33
syrym-almaty <s.zhakypbekov@iitu.edu.kz>
2024-09-13 17:51:29 +0500

    test

--- backend/routes/exampleRoutes.js
+++ backend/routes/exampleRoutes.js
@@ -1,10 +1,24 @@
 // backend/routes/exampleRoutes.js
 const express = require('express');
 const { getExample } = require('../controllers/exampleController');
-
 const router = express.Router();
 
-// Define the route
+/**
+ * @swagger
+ * /api/example:
+ *   get:
+ *     summary: Returns example data
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
 router.get('/example', getExample);
 
 module.exports = router;
